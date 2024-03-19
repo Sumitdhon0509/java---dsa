af37256b4dd6eeb4f677c97e8a6b15d697f4d21c
sumit <dhondikarsumit@gmail.com>
2024-03-19 22:36:39 +0530

    n

---
+++ a.java
@@ -0,0 +1,28 @@
+class a{
+    public static void main(String[] args) {
+   //    while('a'<'b')
+    //   System.out.println("malayalam is a palindrome");
+     //   }
+  //   int x=4,y=0;
+
+  
+  //  while(x>=0){
+   // x--;
+     //   y++;
+       // if(x==y)
+      //  continue;
+        
+        //else
+       // System.out.println(x+""+y);
+    //}
+    int sum = 0;
+
+for (int i = 1; i <= 5; i++) {
+
+    sum += i;
+
+}
+
+System.out.println(sum);
+}
+}
